2f9d0c47fdc51d874f1229b514728949ae1eddec
Alistair Michael <alistair.michael@uq.edu.au>
2024-06-07 18:04:27 +1000

    grammar

--- src/main/scala/ir/Expr.scala
+++ src/main/scala/ir/Expr.scala
@@ -3,6 +3,163 @@ package ir
 import boogie._
 import scala.collection.mutable
 
+enum NType:
+  case Bool
+  case Int
+  case Bitvector(size: Int)
+  case Map(f: NType, t: NType)
+  case Function(arg: List[NType], ret: NType)
+
+enum ScopeDef:
+  case Local
+  case Global
+
+enum UnaryOp(val n: String, val t: NType.Function):
+  case intneg extends UnaryOp("-", NType.Function(List(NType.Int), NType.Int))
+  case bvneg(sz: Int) extends UnaryOp("neg", NType.Function(List(NType.Bitvector(sz)), NType.Bitvector(sz)))
+  case bvnot(sz: Int) extends UnaryOp("not", NType.Function(List(NType.Bitvector(sz)), NType.Bitvector(sz)))
+  case bveq(sz: Int) extends UnaryOp("=", NType.Function(List(NType.Bitvector(sz), NType.Bitvector(sz)), NType.Bool))
+  case boolnot extends UnaryOp("!", NType.Function(List(NType.Bool), NType.Bool))
+  case old(argt: NType) extends UnaryOp("old", NType.Function(List(argt), argt))
+
+enum BinaryOp(val n: String, val t: NType.Function):
+  case bvbinop(sz: Int, name: String) extends BinaryOp(name, NType.Function(List(NType.Bitvector(sz), NType.Bitvector(sz)), NType.Bitvector(sz)))
+  case bvconcat(sz1: Int, sz2: Int) extends BinaryOp("++", NType.Function(List(NType.Bitvector(sz1), NType.Bitvector(sz2)), NType.Bitvector(sz1 + sz2)))
+  case bvcomp(sz: Int) extends BinaryOp("comp", NType.Function(List(NType.Bitvector(sz), NType.Bitvector(sz)), NType.Bitvector(sz)))
+  case bvcompare(sz: Int, name: String) extends BinaryOp(name, NType.Function(List(NType.Bitvector(sz), NType.Bitvector(sz)), NType.Bool))
+  case intbinop(name: String) extends BinaryOp(name, NType.Function(List(NType.Int), NType.Int))
+  case intcompare(name: String) extends BinaryOp(name, NType.Function(List(NType.Int), NType.Bool))
+  case boolbinop(name: String) extends BinaryOp(name, NType.Function(List(NType.Bool, NType.Bool), NType.Bool))
+
+enum RExpr(val t: NType):
+  case Literal(l: LitExpr) extends RExpr(l.t)
+  case ExtractBits(end: Int, start: Int, arg: RExpr) extends RExpr(NType.Bitvector(end - start))
+  case RepeatBits(repeats: Int, arg: RExpr) extends RExpr(NType.Bitvector(repeats * getsize(arg)))
+  case ZeroExtendBits(bits: Int, arg: RExpr) extends RExpr(NType.Bitvector(bits + getsize(arg)))
+  case SignExtendBits(bits: Int, arg: RExpr) extends RExpr(NType.Bitvector(bits + getsize(arg)))
+  case BinaryExpr(op: BinaryOp, arg: RExpr, arg2: RExpr) extends RExpr(op.t.ret)
+  case UnaryExpr(op: UnaryOp, arg: RExpr) extends RExpr(op.t.ret)
+  case Variable(name: String, scope: ScopeDef, shared: Boolean, typ: NType) extends RExpr(typ)
+
+enum LitExpr(val t: NType):
+  case TrueLiteral extends LitExpr(NType.Bool)
+  case FalseLiteral extends LitExpr(NType.Bool)
+  case BitVecLiteral(value: BigInt, s: Int) extends LitExpr(NType.Bitvector(s))
+  case IntLiteral(value: BigInt) extends LitExpr(NType.Int)
+
+/* separating lexprs from exprs makes it simpler to type-safely restrict sub-expressions. */
+enum LExpr(val t: NType):
+  case Variable(name: String, scope: ScopeDef, shared: Boolean, typ: NType) extends LExpr(typ)
+
+enum NStatement:
+  case Assign(v: LExpr, value: RExpr)
+  case Load(dest: LExpr, source: RExpr, ind: RExpr, size: Int, value: RExpr)
+  case Store(v: LExpr, ind: RExpr, value: RExpr)
+  case Assert(value: RExpr)
+  case Assume(value: RExpr)
+  case Return(value: RExpr)
+
+def getsize(b: RExpr) = b.t match 
+  case NType.Bitvector(t) => t
+  case _ => throw Exception("No") 
+
+def vars (e: RExpr) = {
+  var vars = mutable.Set[RExpr.Variable]()
+
+  class VarFinder extends CILVisitorImpl {
+    override def vvar(v: RExpr.Variable) : VisitAction[RExpr.Variable] = {
+      vars.add(v)
+      VisitAction.DoChildren()
+    }
+  }
+}
+
+def toGamma(e: RExpr): RExpr = {
+  e match {
+    case RExpr.Variable(name, scope, shared,_) => RExpr.Variable("Gamma_" + name, scope, shared, NType.Bool)
+    case _ => e
+  }
+}
+
+enum VisitAction[T]:
+  case SkipChildren()
+  case DoChildren()
+  case ChangeTo(e: T) 
+  case ChangeDoChildrenPost(e: T, f: T => T)
+
+trait CILVisitorImpl:
+  def vstmts(e: List[NStatement]): VisitAction[List[NStatement]] = VisitAction.DoChildren()
+  def vstmt(e: NStatement): VisitAction[NStatement] = VisitAction.DoChildren()
+
+  def vvar(e: RExpr.Variable): VisitAction[RExpr.Variable] = VisitAction.DoChildren()
+  def vexpr(e: RExpr): VisitAction[RExpr] = VisitAction.DoChildren()
+
+  def vlexpr(e: LExpr): VisitAction[LExpr] = VisitAction.DoChildren()
+  def vlvar(e: LExpr.Variable): VisitAction[LExpr.Variable] = VisitAction.DoChildren()
+
+class CILVisitor(v: CILVisitorImpl) {
+  import NStatement._ 
+  import RExpr._
+  // TODO: fix evaluation order
+
+  def doVisit[T](v: CILVisitorImpl, a: VisitAction[T], n: T, continue: (CILVisitorImpl, T) => T) : T = {
+    a match {
+      case VisitAction.SkipChildren() => n
+      case VisitAction.DoChildren() => continue(v, n)
+      case VisitAction.ChangeTo(z) => z
+      case VisitAction.ChangeDoChildrenPost(x, f) => f(continue(v, x))
+    }
+  }
+
+  def visit_var(n: RExpr.Variable) : RExpr.Variable = {
+    doVisit(v, v.vvar(n), n, (_, _) => n)
+  }
+
+  def visit_lvar(n: LExpr.Variable) : LExpr.Variable = {
+    doVisit(v, v.vlvar(n), n, (_, _) => n)
+  }
+
+  def visit_lexpr(n: LExpr) : LExpr = {
+    def continue(v: CILVisitorImpl, n: LExpr) : LExpr  = n match {
+      case LExpr.Variable(name: String, scope: ScopeDef, shared: Boolean, typ: NType) => visit_lvar(n.asInstanceOf[LExpr.Variable])
+    }
+    doVisit(v, v.vlexpr(n), n, continue)
+  }
+
+  def visit_expr(n: RExpr) : RExpr = {
+    def continue(v: CILVisitorImpl, n: RExpr) : RExpr  = n match {
+      case RExpr.Literal(l) => n
+      case RExpr.ExtractBits(end: Int, start: Int, arg: RExpr) => RExpr.ExtractBits(end: Int, start: Int, visit_expr(arg))
+      case RExpr.RepeatBits(repeats: Int, arg: RExpr) => RExpr.RepeatBits(repeats: Int, visit_expr(arg))
+      case RExpr.ZeroExtendBits(bits: Int, arg: RExpr) => RExpr.ZeroExtendBits(bits, visit_expr(arg))
+      case RExpr.SignExtendBits(bits: Int, arg: RExpr) => RExpr.SignExtendBits(bits, visit_expr(arg))
+      case RExpr.BinaryExpr(op: BinaryOp, arg: RExpr, arg2: RExpr) => RExpr.BinaryExpr(op, visit_expr(arg), visit_expr(arg2))
+      case RExpr.UnaryExpr(op: UnaryOp, arg: RExpr) => RExpr.UnaryExpr(op, visit_expr(arg))
+      case RExpr.Variable(name: String, scope: ScopeDef, shared: Boolean, typ: NType) => visit_var(n.asInstanceOf[RExpr.Variable])
+    }
+    doVisit(v, v.vexpr(n), n, continue)
+  }
+
+  def visit_stmt(s: NStatement): NStatement = {
+    def continue(v: CILVisitorImpl, n: NStatement) = n match {
+      case Load(dest, source, ind, size, value) => NStatement.Load(visit_lexpr(dest), visit_expr(source), visit_expr(ind), size, visit_expr(value))
+      case Store(v, ind, value) => Store(visit_lexpr(v), visit_expr(ind), visit_expr(value))
+      case Assign(v, value: RExpr) => 
+        val nvalue = visit_expr(value)
+        Assign(visit_lexpr(v), nvalue)
+      case Assert(value) => Assert(visit_expr(value))
+      case Assume(value) => Assume(visit_expr(value))
+      case Return(value) => Return(visit_expr(value))
+    }
+    doVisit(v, v.vstmt(s), s, continue)
+  }
+
+  def visit_stmts(ss: List[NStatement]): List[NStatement] = {
+    def continue (v: CILVisitorImpl, ss: List[NStatement]) = ss.map(visit_stmt)
+    doVisit(v, v.vstmts(ss), ss, continue)
+  }
+}
+
 sealed trait Expr {
   def toBoogie: BExpr
   def toGamma: BExpr = {
@@ -159,6 +316,8 @@ sealed trait BVUnOp(op: String) extends UnOp {
 
 case object BVNOT extends BVUnOp("not")
 case object BVNEG extends BVUnOp("neg")
+
+
 
 case class BinaryExpr(op: BinOp, arg1: Expr, arg2: Expr) extends Expr {
   override def toBoogie: BExpr = BinaryBExpr(op, arg1.toBoogie, arg2.toBoogie)
